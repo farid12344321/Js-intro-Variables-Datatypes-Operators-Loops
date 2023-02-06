f84292c95c3f958890c37eb85b4469e31ad227f1
Faridrab <faridrab@code.edu.az>
2023-02-07 01:59:30 +0400

    js

---
+++ assets/css/script.js
@@ -0,0 +1,156 @@
+"use strict"
+
+
+// console.log("hello p135");
+
+// alert("salam cavid");
+
+
+// var surname ="Ismayilazde";
+
+// let name= "Cavid"
+
+// const IsMarried = true;
+
+
+
+
+// var surname ="Ismayilazde";
+// var name ="Cavid";
+
+
+// console.log(surname + " " + name);
+
+
+// if(true){
+//     var age = 55;
+// }
+
+// console.log(age)
+
+
+// if(true){
+//     let age = 55;
+//     console.log(age)
+// }
+
+
+// const age = 55;
+
+
+// console.log(age)
+
+
+
+// let age =17;
+
+// let name ="Cavid";
+
+// let IsMarried = false;
+
+// let address;
+
+// let phone =null;
+
+// console.log(typeof age);
+
+// console.log(typeof name);
+
+// console.log(typeof IsMarried);
+
+// console.log(typeof address);
+
+// console.log(typeof phone);
+
+
+// if(address == undefined){
+//              console.log("address undefined oldu")
+// }
+
+
+// let stu ={
+//     name:"Cavid",
+//     surname:"Ismayilzade"
+// }
+
+
+// console.log(stu.name)
+
+
+// let students = ["Cavid", "Pervin", "Mirze", 100, 200, true];
+
+// for (let i = 0; i < students.length; i++) {
+// console.log(students[i])
+// }
+
+
+
+//  alert("salamlar");
+
+
+// let username = prompt("please add your username")
+// let IsOnline = confirm("is he online?")
+
+
+// if (IsOnline) {
+//     console.log("he is online")
+// }
+// else {
+//     console.log("he is ofline")
+// }
+
+// debugger;
+
+// console.log(IsOnline)
+
+
+
+// let age = 20;
+
+// if (age > 15) {
+//     console.log("boyukdur")
+// }
+// else {
+//     console.log("kicikdir")
+// }
+
+
+// let temp = 15;
+
+// if (temp < 10) {
+//     console.log("soyuqdur")
+// }
+// else if (temp > 20) {
+//     console.log("istidir")
+// }
+// else {
+//     console.log("hecbisey")
+// }
+
+// let count =5;
+
+// switch(count) {
+//     case 5:
+//       console.log("besh")
+//       break;
+//     case 6:
+//         console.log("alti")
+//       break;
+//     default:
+//         console.log("not found")
+//   }
+
+
+
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i)
+// }
+
+
+
+// let result =2**3;
+
+// console.log(result)
+
+
+// console.log("44" +2);
